96fa999960137e863a6bee775284ab2773fc7052
Maico <marcu123456789@gmail.com>
2018-11-12 21:44:32 +0100

    test

--- webserver.js
+++ webserver.js
@@ -1,26 +1,31 @@
 const http = require('http');
 const fs = require('fs');
 
+var Omx = require('node-omxplayer');
+var player = Omx('my-video.mp4');
+
 const hostname = '192.168.0.69';
 const port = 8080;
 
 const server = http.createServer((req, res) => { //create server
-  fs.readFile(__dirname + '/index.html', function (err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  })
+  if (req.method === "post") {
+    req.on('play', () => {
+      console.log("play");
+    })
+  } else {
+    fs.readFile(__dirname + '/index.html', function (err, data) { //read file index.html in public folder
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
+      res.write(data); //write data from index.html
+      return res.end();
+    })
+  }
 }
 );
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-app.post("/", function (req, res) {
-  console.log("post bekommen");
 });
