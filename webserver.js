0350496b3d29624921729c9ed5eda7c27a9568f9
Maico <marcu123456789@gmail.com>
2018-11-12 22:21:11 +0100

    test

--- webserver.js
+++ webserver.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => { //create server
     }
 
     req.on('end', function() {
-      res.end();
+      return res.status(200).json('success');
     });
 
   } else {
