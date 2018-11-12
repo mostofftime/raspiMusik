05e49669c49c6520559888f8fd1381b764d5607c
Maico <marcu123456789@gmail.com>
2018-11-12 21:31:24 +0100

    test

--- webserver.js
+++ webserver.js
@@ -3,11 +3,18 @@ const http = require('http');
 const hostname = '192.168.0.69';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello there\n');
-});
+const server = http.createServer((req, res) => { //create server
+  fs.readFile(__dirname + '/index.html', function (err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  })
+}
+);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
