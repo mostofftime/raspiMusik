b5936b9c39477af40fac349ea8781222bd32a0cb
Maico <marcu123456789@gmail.com>
2018-11-12 22:28:40 +0100

    test

--- webserver.js
+++ webserver.js
@@ -18,7 +18,16 @@ const server = http.createServer((req, res) => { //create server
     }
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    return res.end();
+    
+    fs.readFile(__dirname + '/index.html', function (err, data) { //read file index.html in public folder
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
+      res.write(data); //write data from index.html
+      return res.end();
+    })
 
 
   } else {
