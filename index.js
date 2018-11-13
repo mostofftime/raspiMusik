f743f14a15099a8442b2763e2f8e99ac46423da1
Maico <marcu123456789@gmail.com>
2018-11-13 14:06:56 +0100

    commit

--- index.js
+++ index.js
@@ -3,19 +3,41 @@ var app = express();
 var path = require('path');
 var Omx = require('node-omxplayer');
 var player = Omx();
+var playing = false;
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     //console.log(path.join(__dirname + '/index.html'));
     //res.send("Hello world!");
 });
 
-app.post('/play', function(req, res){
-    if(!player.running){
-        player.newSource("/media/Musik/10 - Twenty One Pilots - Guns For Hands.mp3");
-    }else{
-        player.play();
+app.post('/play', function (req, res) {
+    if (!playing) {
+        if (!player.running) {
+            player.newSource("/media/Musik/10 - Twenty One Pilots - Guns For Hands.mp3");
+        } else {
+            player.play();
+        }
+    }
+    playing = true;
+});
+
+app.post('/pause', function (req, res) {
+    if (playing) {
+        player.pause();
+    }
+});
+
+app.post('/volUp', function (req, res) {
+    if(player.running){
+        player.volUp();
+    }
+});
+
+app.post('/volDown', function (req, res) {
+    if(player.running){
+        player.volDown();
     }
 });
 
