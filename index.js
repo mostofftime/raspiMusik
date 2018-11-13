033515854db7c8d402b27e6b38ea3d917ff64fc1
Maico <marcu123456789@gmail.com>
2018-11-13 22:43:44 +0100

    commit

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ app.post('/volDown', function (req, res) {
 });
 
 app.post('/song', function(req, res) {
-    console.log(req);
     console.log(req.body);
+    player.newSource("/media/Musik" + req.body.title);
 });
 
 app.listen(8080);
