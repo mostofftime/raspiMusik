d2b7429a63a336077f5c47893a654bf2aced5ae5
Maico <marcu123456789@gmail.com>
2018-11-13 14:47:27 +0100

    commit

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ fs.readdirSync("./media").filter(file => file.endsWith(".mp3")).forEach(file => 
 // viewed at http://localhost:8080
 app.get('/', function (req, res) {
     
-    res.render('index', JSON.stringify(songs));
+    res.render('index');//, JSON.stringify(songs));
     
-    //res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.post('/play', function (req, res) {
