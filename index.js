39608c9d2064548daec1c177d29bf80f218915d3
Maico <marcu123456789@gmail.com>
2018-11-15 23:19:24 +0100

    commit

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ function sorting() {
 
     songs.sort(function (a, b) {
         console.log(songDetails[0]);
-        if (songDetails[songs.indexOf(a)].title < songDetails[songs.indexOf(b)].title) {
+        if (songDetails[songs.indexOf(a)].title < songDetails[songs.indexOf(a)].title) {
             return 1;
         }
-        if (songDetails[songs.indexOf(a)].title > songDetails[songs.indexOf(b)].title) {
+        if (songDetails[songs.indexOf(a)].title > songDetails[songs.indexOf(a)].title) {
             return -1;
         }
         return 0;
